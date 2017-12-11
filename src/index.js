6ebcae7e060afae2ee5a0f77ea747b1df23cc35f
Eric Neo <eric.neo@c02n9fftg3qc.dg.domain.com.au>
2017-12-11 11:42:59 +1100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Timetable from './js/timetable';
+import './scss/index.css';
+import 'react-select/dist/react-select.css';
+
+ReactDOM.render(
+  <Timetable />,
+  document.getElementById('root')
+);
