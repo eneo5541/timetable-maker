688cf0d8d06de27d62f39f1aff3b328d5d8d89fd
Eric Neo <eric.neo@c02n9fftg3qc.dg.domain.com.au>
2017-12-11 15:45:35 +1100

    Adding hover state for incomplete events

--- js/room.js
+++ js/room.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import Event from './event';
-import { formatAMPM } from './timeUtils';
 
 class Room extends React.Component {
   state = {
     events: [],
     editEvent: undefined,
+    hoverTime: undefined,
     hexColor: '#'+Math.floor(Math.random()*16777215).toString(16),
-  }
-
-  eventDeleted = (eventId) => {
-    this.setState({ events: this.state.events.filter(event => event.id !== eventId) });
   }
 
   toggleEvent = (event, time) => {
@@ -18,21 +14,43 @@ class Room extends React.Component {
     const element = event.target;
     if (!eventExists) {
       if (this.state.editEvent) {
-        const newCoords = this.state.editEvent.coords.concat([element.offsetLeft + element.offsetWidth]).sort((a, b) => a - b);
+        const newCoords = this.state.editEvent.coords.concat([element.offsetLeft]).sort((a, b) => a - b);
         const newEvent = Object.assign({}, this.state.editEvent, { finishTime: time, coords: newCoords });
         this.setState({ 
           events: this.state.events.concat([newEvent]),
           editEvent: undefined,
+          hoverTime: undefined,
         });
       } else {
         const newEvent = {
           id: `${this.props.roomId}-event-${new Date().getTime()}`,
           startTime: time,
           coords: [ element.offsetLeft ],
+          width: element.offsetWidth,
         };
         this.setState({ editEvent: newEvent });
       }
     }
+  }
+
+  eventDeleted = (eventId) => {
+    this.setState({ events: this.state.events.filter(event => event.id !== eventId) });
+  }
+
+  mouseOverHandler = (interval) => {
+    if (this.state.editEvent) {
+      this.setState({ hoverTime: interval });
+    }
+  }
+
+  displayHover = (interval) => {
+    if (this.state.editEvent) {
+      const times = [this.state.editEvent.startTime, this.state.hoverTime].sort((a, b) => a - b);
+      if (interval >= times[0] && interval <= times[1]) {
+        return true;
+      }
+    }
+    return false;
   }
 
   render() {
@@ -41,16 +59,21 @@ class Room extends React.Component {
         <div className="timetable-room-label">
           {this.props.room.label}
           <button className="timetable-toggle-button" onClick={() => this.props.roomDeleted(this.props.room)}>
-            &#10062;
+            <span role="img" aria-label="delete room">&#10062;</span>
           </button>
         </div>
         <div className="timetable-schedules-time-labels">
           {this.props.intervals.map(interval => (
             <div
               key={interval.value}
-              className={`timetable-time`} 
+              className={`timetable-time`}
               onClick={(event) => this.toggleEvent(event, interval.value)}
-            />
+              onMouseOver={() => this.mouseOverHandler(interval.value)}
+            >
+              {this.displayHover(interval.value) && 
+                <div className="timetable-time-hover-overlay" />
+              }
+            </div>
           ))}
 
           <div className="timetable-schedules-room-events">
